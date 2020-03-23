b39a9e975c5d7e9eb39bfcbef2ec90fe7a3e755e
mmshareef <muhammadmuadassirshareef@gmail.com>
2020-03-23 17:08:30 +0530

    cooments added

--- sudokusolver.js
+++ sudokusolver.js
@@ -1,4 +1,6 @@
 //sudoku solver
+// zeros are indicitive of empty space
+// number should not be repeated in the same row, column , or in the subgrid
 var sudoko = [
     [0, 0, 0, 2, 6, 0, 7, 0, 1],
     [6, 8, 0, 0, 7, 0, 0, 9, 0],
@@ -9,16 +11,17 @@ var sudoko = [
     [0, 0, 9, 3, 0, 0, 0, 7, 4],
     [0, 4, 0, 0, 5, 0, 0, 3, 6],
     [7, 0, 3, 0, 1, 8, 0, 0 ,0]
-];
+]; 
+
 function solveGame(arr){
-var x = {}, noNum, total = 81;
-while(total > 0){
-total =0;
+var x = {}, noNum, total = 81;// total - 9*9=81 spaces, x - object to keep track
+while(total > 0){// loop ends when there are no empty spaces left
+total =0;// incemented every single time an empty space is found
 for(var r = 0; r < arr.length; r++){
 for(var h = 0; h < arr.length; h++){
-x = {};
-if(arr[r][h] === 0){
-   for(var i = 0; i < 9; i++){
+x = {};// used to keep track of all the possible numbers that cannot be put at each empty space
+if(arr[r][h] === 0){ // if we find a zero at any space or element
+   for(var i = 0; i < 9; i++){ // iterates through zero to nine
        // Checking for each row elements possibilty
        if(arr[r][i] > 0){
            x[arr[r][i]] = 1;
@@ -29,7 +32,7 @@ if(arr[r][h] === 0){
        }
    }
    // Checking for 3*3 grid elements possibiltygit 
-   for(var rgrid = Math.floor(r / 3) * 3; rgrid < Math.floor(r / 3) * 3 + 3; rgrid++){
+   for(var rgrid = Math.floor(r / 3) * 3; rgrid < Math.floor(r / 3) * 3 + 3; rgrid++){ // floor rounds it down
        for(var hgrid = Math.floor(h / 3) * 3; hgrid < Math.floor(h / 3) * 3 + 3; hgrid++){
            if(arr[rgrid][hgrid]){
                x[arr[rgrid][hgrid]] = 1;
@@ -37,8 +40,8 @@ if(arr[r][h] === 0){
        }
    }
    // Replacing the zeros with  possible numbers
-   noNum = Object.keys(x);
-   if(noNum.length === 8){
+   noNum = Object.keys(x);// object will be in form of strings. 
+   if(noNum.length === 8){ // if there are 8 values in x then the remaining one is the solution
        for(var i = 0; i < 10; i++){
            if(noNum.indexOf(i.toString()) < 0){
                arr[r][h] = i;
